f96e7ce2b5dcecedd5390fd271f96c847aebb6df
saisagar93 <sscreator225@gmail.com>
2020-12-20 14:55:58 +0530

    Add:-code to won 21 times

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Welcome FlipCoin Problem"
 
@@ -6,20 +6,29 @@ heads=0
 tails=1
 headCount=0
 tailCount=0
-for (( i=1; i<=20; i++ ))
+for (( i=1; i>0; i++ ))
 do
 
 	coin=$((RANDOM%2));
 	if [ $coin -eq 1 ];
 	then
 		headCount=$(( $headCount + 1 ))
+		if [ $headCount -eq 21 ]
+		then
+			result=$((headCount-tailCount))
+			echo "heads win by $result diff";
+			exit
+		fi
 	fi
 
 	if [ $coin -eq 0 ];
         then
                	tailCount=$(( $tailCount + 1 ))
+                if [ $tailCount -eq 21 ]
+		then
+			result=$((tailCount-headCount))
+                	echo "tails win by $result diff";
+                	exit
+		fi
         fi
 done
-
-echo "heads is $headCount"
-echo "tails is $tailCount"
