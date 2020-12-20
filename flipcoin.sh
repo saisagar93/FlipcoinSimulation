494d28c2e274294002187875f3b3d64cd9fca1a1
saisagar93 <sscreator225@gmail.com>
2020-12-20 14:51:31 +0530

    Add:-code show number of times head and tail

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,11 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome FlipCoin Problem"
 
-coin=$(( RANDOM %2 + 1 ));
-if [ $coin -eq 1 ];
-then
-	echo "heads win";
-else
-	echo "tails win";
-fi
+heads=0
+tails=1
+headCount=0
+tailCount=0
+for (( i=1; i<=20; i++ ))
+do
+
+	coin=$((RANDOM%2));
+	if [ $coin -eq 1 ];
+	then
+		headCount=$(( $headCount + 1 ))
+	fi
+
+	if [ $coin -eq 0 ];
+        then
+               	tailCount=$(( $tailCount + 1 ))
+        fi
+done
+
+echo "heads is $headCount"
+echo "tails is $tailCount"
